113e99ab648ca2d9e2c1805d013a834d9af607d4
DarioGuastella <jimmyguastella@gmail.com>
2023-11-30 15:43:53 +0100

    stampa cards senza stile

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <script>
 import axios from 'axios';
 import { store } from "./store.js"
+import BeerCard from "./components/BeerCard.vue"
 export default {
   components: {
+    BeerCard
   },
   data() {
     return {
+      store,
     }
   },
   mounted() {
+    this.getBeers();
+    console.log(this.store)
   },
   methods: {
-
+    getBeers() {
+      axios.get(this.store.apiUrl).then(result => {
+        this.store.beers = result.data;
+      });
+    }
   }
 }
 </script>
 
-<template></template>
+<template>
+  <BeerCard v-for="beer in store.beers" :beer="beer" />
+</template>
 
 <style scoped></style>
---
+++ src/components/BeerCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "BeerCard",
+    props: {
+        beer: Object
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div>
+            <h2>{{ beer.name }}</h2>
+            <h4>{{ beer.country }}</h4>
+            <h4>{{ beer.city }}</h4>
+
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
