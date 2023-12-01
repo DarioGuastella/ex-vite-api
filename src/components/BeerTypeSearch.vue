ac0abaf919a4ae6be67ffb6a3bcc90f9149d0b29
DarioGuastella <jimmyguastella@gmail.com>
2023-12-01 15:46:27 +0100

    feat: rimuove categorie vuote

--- src/components/BeerTypeSearch.vue
+++ src/components/BeerTypeSearch.vue
@@ -25,17 +25,11 @@ export default {
         <label for="beers">Scegli una tipologia di birra:</label>
 
         <select v-model="store.beersType" name="beers" id="beers">
-            <option value="">Tutte le tipologie</option>
+            <option value="">--Seleziona--</option>
             <option value="micro">Micro</option>
-            <option value="nano">Nano</option>
             <option value="regional">Regional</option>
             <option value="brewpub">Brewpub</option>
             <option value="large">Large</option>
-            <option value="planning">Planning</option>
-            <option value="bar">Bar</option>
-            <option value="contract">Contract</option>
-            <option value="proprietor">Proprietor</option>
-            <option value="closed">Closed</option>
         </select>
         <button @click="search">Cerca</button> <!-- Rigo 14 -->
     </div>
